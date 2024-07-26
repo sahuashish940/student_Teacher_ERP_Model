91aa6674f941d98799825a956219f22163fe3469
Ashish sahu <112854665+sahuashish940@users.noreply.github.com>
2024-07-26 22:54:51 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-he
+document.getElementById('sign-up-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  const password = document.getElementById('password').value;
+  const confirmPassword = document.getElementById('confirm-password').value;
+
+  if (password !== confirmPassword) {
+      alert('Passwords do not match!');
+      return;
+  }
+
+  alert('Form submitted successfully!');
+});
+
+// login.html javascrit
+
+document.getElementById('login-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  // Add your login logic here
+  alert('Login form submitted!');
+});
